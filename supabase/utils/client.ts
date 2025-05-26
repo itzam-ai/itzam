2a6089c1088f3850514d8a7c66b53a65768f0f19
“Abdul <abdulhdr1@gmail.com>
2025-05-26 15:54:21 -0300

    Enhance Supabase client type safety
    - Updated the createClient function to specify the Database type for improved type safety when creating the Supabase client.

--- supabase/utils/client.ts
+++ supabase/utils/client.ts
@@ -1,7 +1,9 @@
 import { env } from "@itzam/utils/env";
 import { createBrowserClient } from "@supabase/ssr";
+import { Database } from "../functions/_shared/database.types";
+
 export function createClient() {
-  return createBrowserClient(
+  return createBrowserClient<Database>(
     env.NEXT_PUBLIC_SUPABASE_URL,
     env.NEXT_PUBLIC_SUPABASE_ANON_KEY
   );
