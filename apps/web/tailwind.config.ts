bf222cfa744be9d48751c4ca215c073929cc5fbf
Abdul Haidar <abdulhdr1@gmail.com>
2025-06-24 19:49:07 -0300

    g

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -99,11 +99,21 @@ export default {
             opacity: "0",
           },
         },
+        "star-movement-bottom": {
+          "0%": { transform: "translate(0%, 0%)", opacity: "1" },
+          "100%": { transform: "translate(-100%, 0%)", opacity: "0" },
+        },
+        "star-movement-top": {
+          "0%": { transform: "translate(0%, 0%)", opacity: "1" },
+          "100%": { transform: "translate(100%, 0%)", opacity: "0" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         "fade-in": "fade-in 0.2s ease-in-out",
+        "star-movement-bottom": "star-movement-bottom linear infinite alternate",
+        "star-movement-top": "star-movement-top linear infinite alternate",
       },
     },
   },
