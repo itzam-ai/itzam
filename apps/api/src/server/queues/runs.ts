4a449a8619b1c2988fada2d40cae4b25f4c35f91
Abdul Haidar <abdulhdr1@gmail.com>
2025-06-27 22:36:38 -0300

    Update dependencies and add job queue for run processing
    - Updated `pnpm-lock.yaml` and `package.json` to include new dependencies for Bull and its TypeScript types.
    - Introduced a new file `runs.ts` for managing job queues using Bull, enabling asynchronous processing of run tasks.
    - Enhanced type safety with TypeScript interfaces for run data and attachments.
    - Implemented job processing logic to handle database operations for runs and attachments.

---
+++ apps/api/src/server/queues/runs.ts
@@ -0,0 +1,26 @@
+import { db } from "@itzam/server/db/index";
+import { attachments, runs } from "@itzam/server/db/schema";
+import Queue from "bull";
+
+type NewRun = typeof runs.$inferSelect & {
+	attachments: (typeof attachments.$inferSelect)[];
+};
+
+const newRunQueue = new Queue<NewRun>("create-run");
+
+export const createRun = async (run: NewRun) => {
+	const job = await newRunQueue.add(run);
+	return job;
+};
+
+newRunQueue.process(async (job) => {
+	const run = await db.insert(runs).values(job.data).returning();
+
+	if (job.data.attachments && job.data.attachments.length > 0) {
+		await db.insert(attachments).values(job.data.attachments);
+	}
+
+	if (!run) {
+		throw new Error("Run not found");
+	}
+});
